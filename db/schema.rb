575b80aecb3e45976fdaf52aa9ffe7d1f39b2216
Hyeon Kim <simnalamburt@gmail.com>
2015-04-25 00:33:51 +0900

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401160627) do
+ActiveRecord::Schema.define(version: 20150424153151) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
