4e7e07a894b644d5b5117026b902bba284a942ea
Kim, Jae Chan <kjc0210@gmail.com>
2015-04-26 00:50:26 +0900

    [Issue #12] test for search course

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  include Devise::TestHelpers
+
+  test 'course list should get result' do
+    @user = users(:one)
+    sign_in :user, @user
+
+    get :course_list
     assert_response :success
+    json = JSON.parse(response.body)
+    assert_equal 6, json.length
   end
 
+  test 'unauthenticated user cannot see course list' do
+    get :course_list
+    assert_response :redirect
+  end
 end
