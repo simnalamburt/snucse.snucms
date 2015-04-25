49d0f318de5d8ca748dcb8960d29af6653ae48ac
Kim, Jae Chan <kjc0210@gmail.com>
2015-04-26 04:07:31 +0900

    [Issue #10] Add test for unfollow course

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -61,4 +61,40 @@ class HomeControllerTest < ActionController::TestCase
     post :follow_course, {id: 10}
     assert_response 400
   end
+
+  # Unfollowing course test
+
+  test 'user can unfollow followed course' do
+    user = users(:one)
+    sign_in :user, user
+
+    post :follow_course, {id: 1}
+    post :follow_course, {id: 2}
+
+    delete :unfollow_course, {id: 1}
+    assert_response :success
+    json = JSON.parse(response.body)
+    assert_equal 1, json.length
+    assert_not json.map{ |j| j['id'] }.include? 1
+  end
+
+  test 'user cannot unfollow unfollowed course' do
+    user = users(:one)
+    sign_in :user, user
+
+    post :follow_course, {id: 2}
+
+    delete :unfollow_course, {id: 1}
+    assert_response 400
+  end
+
+  test 'ser cannot unfollow invalid course' do
+    user = users(:one)
+    sign_in :user, user
+
+    post :follow_course, {id: 1}
+
+    delete :unfollow_course, {id: 'aaa'}
+    assert_response 400
+  end
 end
