74ef2b139af130f2d7f8934666700f17a6e360ff
Kim, Jae Chan <kjc0210@gmail.com>
2015-04-26 01:55:17 +0900

    [issue #10] Implement add course api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,4 +8,15 @@ class HomeController < ApplicationController
     courses = Course.all
     render json: courses.map { |c| { id: c.id, 'title' => c.name } }
   end
+
+  def follow_course
+    @course = Course.find(params[:id])
+    return render nothing: true, status: 400 if @course.nil?
+
+    return render nothing: true, status: 409 if current_user.course.include? @course
+
+    current_user.course << @course
+
+    render json: current_user.course
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   end
 
   get 'course/list' => 'home#course_list'
+  post 'course/follow' => 'home#follow_course'
 end
