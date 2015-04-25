0ceaecd2ace16e1591fb88a8f8298d5a9535ab24
Kim, Jae Chan <kjc0210@gmail.com>
2015-04-26 00:23:09 +0900

    [Issue #12] Make simple example for onSelect callback for search

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,10 @@
   $.get('/course/list')
   .success(function(data) {
     $('.ui.search').search({
-      source: data
+      source: data,
+      onSelect: function(result) {
+        console.log(result);
+      }
     });
   });
 }).call(this);
