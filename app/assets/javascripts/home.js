4e9ea12d03ad3f70b99d6269bbe250bf8f19b1a1
Hyeon Kim <simnalamburt@gmail.com>
2015-05-04 00:39:25 +0900

    Refactoring follow/unfollow codes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,40 +1,34 @@
-(function() {
-  $(document).ready(function() {
-    $.get('/courses.json')
-    .success(function(data) {
-      $('.ui.search').search({
-        source: data.map(function(i) { return { id: i.id, title: i.name } }),
-        onSelect: function(result) {
-          $.post('/course/follow', {id: result.id})
-          .success(function() {
-            $("#course-list").append(
-              '<p class="item">' +
-                result.title +
-                '<a href="#" data-course-id="' +
-                  result.id +
-                  '" class="delete-course">' +
-                  '<i class="icon trash"></i>' +
-                '</a>' +
-              '</p>'
-              );
-            $(".ui.search .prompt").val('');
-          })
-          .error(function() {
-          });
-        }
-      });
-    });
-
-    $(document).on('click', ".delete-course", function() {
-      var $this = $(this);
-      $.ajax({
-        method: 'delete',
-        url: '/course/unfollow',
-        data: {id: $this.data('courseId')}
-      }).success(function(data) {
-        $this.parents('p.item').remove();
-      });
-      return false;
+$(function() {
+  $.get('/courses.json').success(function(data) {
+    $('.ui.search').search({
+      source: data.map(function(i) { return { id: i.id, title: i.name } }),
+      onSelect: function(result) {
+        $.post('/course/follow', { id: result.id }).success(function() {
+          $('#course-list').append(
+            '<p class="item">' +
+              result.title +
+              '<a href="#" data-course-id="' +
+                result.id +
+                '" class="delete-course">' +
+                '<i class="icon trash"></i>' +
+              '</a>' +
+            '</p>'
+            );
+          $('.ui.search .prompt').val('');
+        });
+      }
     });
   });
-})();
+});
+
+$(document).on('click', '.delete-course', function() {
+  var $this = $(this);
+  $.ajax({
+    method: 'delete',
+    url: '/course/unfollow',
+    data: { id: $this.data('courseId') }
+  }).success(function() {
+    $this.parents('p.item').remove();
+  });
+  return false;
+});
