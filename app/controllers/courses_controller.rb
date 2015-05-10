ca2085e34fe591a12c4e659c83052cbc2d5b329a
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-10 19:42:34 +0900

    pass test

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :authenticate_admin!, except: [:index]
+  before_action :authenticate_user!, only: [:index]
   before_action :set_course, only: [:show, :edit, :update, :destroy]
 
   # GET /courses
