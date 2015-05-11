50636987f49197f56634efca6e8d5ade99c15b5d
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-11 14:46:03 +0900

    add validation for site model

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,16 @@
+
 class Site < ActiveRecord::Base
+  CRAWLING_TYPES = %w(googlesite xe gnuboard phpbb custom)
+
   belongs_to :course
+  # http://stackoverflow.com/questions/4434451/rails-equivalent-to-djangos-choices
+  validates_inclusion_of :crawling_type, in: CRAWLING_TYPES,
+    message: "%{value} is not in valid crawling type. It must in #{CRAWLING_TYPES}"
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :course_id, presence: true
+
+  def init
+    self.crawling_type ||= 'custom'
+  end
 end
