7945fef8c432af1f364133af459a4d22a230dcbe
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-13 03:03:54 +0900

    make test for timeline

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,23 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "timeline_initial" do
+    @user.course = [courses(:one), courses(:two)]
+    timeline = @user.timeline_initial(2)
+    assert_equal 2, timeline.count
+    assert_equal 5, timeline[0].id
+    assert_equal 4, timeline[1].id
+  end
+
+  test "timeline_older_than" do
+    @user.course = [courses(:one), courses(:two)]
+    timeline = @user.timeline_older_than 5
+    assert_equal 4, timeline.count
+    timeline = @user.timeline_older_than 1
+    assert_equal 0, timeline.count
+  end
 end
