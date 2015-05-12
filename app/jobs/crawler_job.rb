06cb72be4fae7bd0bca4a03d2c0e7306e35de68a
Hyeon Kim <simnalamburt@gmail.com>
2015-05-13 08:57:43 +0900

    Use crawled data

--- app/jobs/crawler_job.rb
+++ app/jobs/crawler_job.rb
@@ -1,21 +1,24 @@
 class CrawlerJob < ActiveJob::Base
   def perform
-    messages = <<-MESSAGES
-3/4: Office hours changed from <strike>TuTh 11AM-12PM</strike> to MW 1PM-2PM
-3/11: Class for 3/12 (Thu) will be given at <b>Bldg. 302, Rm. 311-1</b> (practice session)
-3/16: Class for 3/16 (Today) will be given at <b>Bldg. 302, Rm. 106 (</b>proposal presentation & team formation)
-3/17:  Individual warm-up project due 4/10 (Fri) 6PM
-4/27: Class for 4/27 (Today) will be given at <b>Bldg. 302, Rm. 106</b> (iteration 1 presentation with pizza)
-5/1: No class on 5/5 (Tue)
-5/10: Class for 5/11 (Mon) will be given at <b>Bldg. 302, Rm. 106</b> (lecture)
-    MESSAGES
+    entries = Site.google_site_recent_changes.flat_map do |site|
+      result = Wombat.crawl do
+        base_url site.url
 
-    site = Course.find_by(name: '소프트웨어 개발의 원리와 실제').sites[0]
-    pivot_date = DateTime.new(2015, 3, 1, 0, 0, 0)
+        activities({ css: '#sites-recent-activity-wrapper tr' }, :iterator) do
+          date css: 'td:nth(1) span'
+          msg({ css: 'td:nth(2)' }, :html)
+        end
+      end
 
-    inputs = messages.lines.map do |l|
-      { message: l.chomp, site: site, created_at: pivot_date.change(day: 3, hour: 13, min: 3, sec: 18) }
+      result['activities'].map do |action|
+        {
+          message: action['msg'],
+          site: site,
+          created_at: Date.parse(action['date'])
+        }
+      end
     end
-    Log.create inputs
+
+    Log.create entries
   end
 end
