9ac528146f55249d57341a9075e0126d02949c77
es-lee <dldms800@gmail.com>
2015-05-13 03:09:45 +0900

    cucumber user step definition(logout)

--- features/user_steps.rb
+++ features/user_steps.rb
@@ -10,7 +10,7 @@ When /^I want to sign up, I fill in "(.*)", "(\w+)" with "(\w+)"$/ do |email, pw
 end
 
 Then /^I should be on the SNUCSE CMS main page$/ do
-  assert page.has_content? 'Signed in successfully'
+  assert page.has_content?('Signed in successfully')#ㄱㅊㄱ
 end
 
 When /^I want to log in, I fill in "(.*)" with "(.*?)"$/ do |email, pwd|
@@ -19,5 +19,14 @@ When /^I want to log in, I fill in "(.*)" with "(.*?)"$/ do |email, pwd|
   click_button 'login_button'
 end
 
-When /^I press "Logout" button$/
+Given /^I am on the SNUCSE CMS main page$/ do
+  assert page.has_content?('P')
+end
+
+When /^I press "Logout" button$/ do
+  click_button 'logout_button'
+end
+
+Then /^I should go to SNUCSE CMS landing page$/ do
+  assert page.has_content?('login')#고치기
 end
