ee942078fbeb4ac2b028a382be6069984638f2ae
khm <kalaluthien@naver.com>
2015-05-25 15:06:51 +0900

    add new column 'code' to courses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513001055) do
+ActiveRecord::Schema.define(version: 20150525060133) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150513001055) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "code"
   end
 
   create_table "courses_users", id: false, force: :cascade do |t|
