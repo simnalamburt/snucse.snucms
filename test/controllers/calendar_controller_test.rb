887d30ce06c1e6671750adc23c003a8697b5c0a4
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-25 20:08:24 +0900

    add test for rendering calendar

--- test/controllers/calendar_controller_test.rb
+++ test/controllers/calendar_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class CalendarControllerTest < ActionController::TestCase
-#  test "should get index" do
-#    get :index
-#    assert_response :success
-#  end
+  include Devise::TestHelpers
+
+  setup do
+    @today = Date.today
+    @year = @today.year
+    @month = @today.month
+    @start_wday = @today.at_beginning_of_month.wday
+
+    @user = users(:one)
+    sign_in :user, @user
+  end
+
+  test "should get index" do
+    get :index
+    doc = Nokogiri::HTML response.body
+    assert_equal 1, doc.search("table.calendar td")[@start_wday].text.to_i
+  end
 
 end
