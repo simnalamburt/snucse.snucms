b2dc5fb750619f246a86629e1bb5f8de9d9fbc3c
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-26 16:09:27 +0900

    add tests for schedule's comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,5 +4,4 @@ class Comment < ActiveRecord::Base
   validates :schedule_id, presence: true
   validates :user_id, presence: true
   validates :content, presence: true
-  validates :created_at, presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @schedule = schedules(:one)
+    @user = users(:one)
+  end
+
+  test "each schedule has comments" do
+    assert_equal 3, @schedule.comments.count
+    assert_equal 2, schedules(:two).comments.count
+    assert_equal 3, @user.comments.count
+  end
+
+  test "add comment to schedule" do
+    Comment.create!(schedule: @schedule, user: @user, content: "comment")
+    assert_equal 4, @schedule.reload.comments.count
+  end
+
+  test "remove comment of schedule" do
+    @schedule.comments[0].destroy
+    assert_equal 2, @schedule.comments.count
+
+    Comment.where(user: users(:two)).destroy_all
+    assert_equal 0, users(:two).comments.count
+  end
 end
