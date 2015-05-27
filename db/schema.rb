6995dbb6b7f854364e3d450b66b25950ade4921c
Kim, Jae Chan <kjc0210@gmail.com>
2015-05-27 09:46:26 +0900

    Add due_time field in schedule model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526184644) do
+ActiveRecord::Schema.define(version: 20150527004115) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20150526184644) do
     t.string   "name"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "due_time"
   end
 
   add_index "schedules", ["course_id"], name: "index_schedules_on_course_id"
