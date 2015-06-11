90861a15d3bcc4f0fa1710d5d8b1ab5f8a3fedc7
Hyeon Kim <simnalamburt@gmail.com>
2015-06-12 00:10:49 +0900

    crawler_job.rb 업데이트

--- app/jobs/crawler_job.rb
+++ app/jobs/crawler_job.rb
@@ -3,25 +3,18 @@ class CrawlerJob < ActiveJob::Base
     entries = Site.google_site_recent_changes.flat_map do |site|
       result = Wombat.crawl do
         base_url site.url
-
         activities({ css: '#sites-recent-activity-wrapper tr' }, :iterator) do
-          date do
-            date_ge_one_day xpath: 'td[1]/strong/span'
-            date_lt_one_day xpath: 'td[1]/strong/noscript'
-          end
+          date0 xpath: 'td[1]/strong/noscript'
+          date1 xpath: 'td[1]/strong/span'
           msg({ css: 'td:nth(2)' }, :html)
         end
       end
 
       result['activities'].map do |action|
         {
-          message: action['msg'],
           site: site,
-          created_at: DateTime.parse(
-            (action['date'])['date_ge_one_day'].nil? ?
-            (action['date'])['date_lt_one_day'] :
-            (action['date'])['date_ge_one_day']
-          )
+          message: action['msg'],
+          created_at: DateTime.parse(action['date0'] || action['date1'])
         }
       end
     end
